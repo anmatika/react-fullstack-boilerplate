387c99a1a9e71d4a2dbeedc2616bd251080e6c86
Jonas Jensen <jonasj@mesan.no>
2016-12-25 15:22:45 +0100

    update app to use single express instance and host API on same application port, mount api on /api

--- src/server/index.js
+++ src/server/index.js
@@ -2,27 +2,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const api = require('./api');
 const webpackConfig = require('../client/dev.webpack.config.js');
 const buildPath = path.resolve(__dirname, "..", "client", "build");
 
+const compiler = webpack(webpackConfig);
+const devMiddleware = webpackDevMiddleware(compiler, {
+    stats: {
+        colors: true,
+        children: false,
+        reasons: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false
+    }
+});
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(api);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Listening on port :" + port));
-
-const compiler = webpack(webpackConfig);
-const server = new WebpackDevServer(compiler, {hot: true});
-server.listen(8080);
+app.use('/api', api);
+app.use(devMiddleware);
+app.use(webpackHotMiddleware(compiler));
 
 app.get('*', (req, res) => {
     const generatedIndexHtmlPath = path.resolve(buildPath, 'index.html');
-    res.write(middleware.fileSystem.readFileSync(generatedIndexHtmlPath));
+    res.write(devMiddleware.fileSystem.readFileSync(generatedIndexHtmlPath));
     res.end();
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Listening on port :" + port));
